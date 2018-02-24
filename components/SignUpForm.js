3a3c2b829cbfedcde26643e62f19977f5fced8e5
Inseong Park <rarira@gmail.com>
2018-02-24 18:31:32 +0900

    lec 70 finished

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { FormLabel, Button, FormInput } from 'react-native-elements';
+import axios from 'axios';
+
+const ROOT_URL = 'https://us-central1-one-time-password-c17ec.cloudfunctions.net';
 
 export default class SignUpForm extends Component {
   state = { phone: '' };
+
+  handleSubmit = async () => {
+    const { phone } = this.state;
+    try {
+      await axios.post(`${ROOT_URL}/createUser`, { phone });
+      await axios.post(`${ROOT_URL}/requestOneTimePassword`, { phone });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   render() {
     return (
@@ -12,11 +25,14 @@ export default class SignUpForm extends Component {
           <FormLabel>Enter Phone Number</FormLabel>
           <FormInput
             value={this.state.phone}
-            onChangeText={phone => this.setState({ phone })} 
+            onChangeText={phone => this.setState({ phone })}
           />
         </View>
 
-        <Button title="Submit" />
+        <Button
+          onPress={this.handleSubmit}
+          title="Submit"
+        />
       </View>
     );
   }
